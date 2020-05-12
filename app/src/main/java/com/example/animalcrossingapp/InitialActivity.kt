c0d26795b9ced8dd27e539b771888d99c8575245
haxduck@naver.com <glfhtmdp07>
2020-05-12 10:20:33 +0900

    First commit

---
+++ app/src/main/java/com/example/animalcrossingapp/InitialActivity.kt
@@ -0,0 +1,15 @@
+package com.example.animalcrossingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class InitialActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_initial)
+    }
+
+
+}
--- app/src/main/java/com/example/animalcrossingapp/MainActivity.kt
+++ app/src/main/java/com/example/animalcrossingapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.animalcrossingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        var flag: Int = 0
+        if(flag == 0) {
+            setContentView(R.layout.activity_initial)
+        } else {
+            setContentView(R.layout.activity_main)
+        }
+
     }
+
 }
